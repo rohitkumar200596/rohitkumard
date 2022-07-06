04c5dec8dac472136d28fbd16908f666e2d700bd
rohitkumar200596 <108667237+rohitkumar200596@users.noreply.github.com>
2022-07-06 22:41:11 +0530

    Updates

---
+++ precache-manifest.1c7adf1711706433d3831c78b6922bcc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d3dd13a86375586d351fcea17ee2be",
+    "url": "/rohitkumard/index.html"
+  },
+  {
+    "revision": "e46acd454f01d1e787d1",
+    "url": "/rohitkumard/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "72001275ce33c4e87167",
+    "url": "/rohitkumard/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "a932a6755c2c5e2a0f3a",
+    "url": "/rohitkumard/static/css/main.28519464.chunk.css"
+  },
+  {
+    "revision": "47d06b028108b25b7293",
+    "url": "/rohitkumard/static/js/2.d41489aa.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/rohitkumard/static/js/2.d41489aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46acd454f01d1e787d1",
+    "url": "/rohitkumard/static/js/3.3be6c379.chunk.js"
+  },
+  {
+    "revision": "72001275ce33c4e87167",
+    "url": "/rohitkumard/static/js/4.22f51896.chunk.js"
+  },
+  {
+    "revision": "a932a6755c2c5e2a0f3a",
+    "url": "/rohitkumard/static/js/main.fa4d5953.chunk.js"
+  },
+  {
+    "revision": "a426f88dc25926849423",
+    "url": "/rohitkumard/static/js/runtime-main.ef79d054.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/rohitkumard/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "2c5590a4e75084d036208e48315ba289",
+    "url": "/rohitkumard/static/media/GPM.2c5590a4.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/rohitkumard/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f69bce8ad8716a38d8a2750b9823d8fc",
+    "url": "/rohitkumard/static/media/N. G. Acharya & D. K. Marathe College.f69bce8a.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/rohitkumard/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/rohitkumard/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/rohitkumard/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/rohitkumard/static/media/skill.34409398.svg"
+  }
+]);
